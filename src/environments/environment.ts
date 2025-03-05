c4475179908e5a41c956919259ef296ff447354a
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2025-03-05 18:25:33 +0000

    fix(env): Temporary fix hard-coding api-urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,9 @@
+
 const production_environment = {
   production: true,
-  apiUrl: process.env['API_URL'],
-  apiVersion: process.env['API_VERSION'],
-  webSocketUrl: process.env['WEBSOCKET_URL']
+  apiUrl: 'https://inclined-ellette-antapagon-labs-71b96869.koyeb.app',
+  apiVersion: 'api/v1',
+  webSocketUrl: 'ws://inclined-ellette-antapagon-labs-71b96869.koyeb.app'
 };
-
-
-console.warn("This is a production build. The environment production are: ", production_environment);
-console.warn("Environment variables: ", process.env)
-console.warn("Environment variables: ", process)
 
 export const environment = production_environment;
