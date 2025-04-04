028959ce07ed33415bd3b447c6b7b26dec21d0ef
antonioap101 <antonioap101@users.noreply.github.com>
2025-04-04 16:52:52 +0100

    refactor: apply @EmitAfter decorator in ICListen streaming config panel
    Refactored ICListenStreamingConfigMonitorPanelComponent to use @EmitAfter decorator
    for all data mutations. All ngModel bindings now route through dedicated setter methods
    to ensure emitChange(this.data) is called automatically after changes.
    
    This improves maintainability and ensures consistent update emissions.

--- src/app/components/node-monitor-panel/pam-modules/iclisten/iclisten-logging-config-monitor-panel/iclisten-data-collection-config-monitor-panel.component.ts
+++ src/app/components/node-monitor-panel/pam-modules/iclisten/iclisten-logging-config-monitor-panel/iclisten-data-collection-config-monitor-panel.component.ts
@@ -1,11 +1,14 @@
+// iclisten-data-collection-config-monitor-panel.component.ts
 import {Component} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
-import {TranslateModule} from "@ngx-translate/core";
-import {ICListenLoggingConfig} from "@/app/global-interfaces/nodes/PamModules";
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {ICListenLoggingConfig} from '@/app/global-interfaces/nodes/PamModules';
+
 import {
+  EmitChangesAfter,
   MutableNodeMonitorPanelComponent
-} from "@/app/components/node-monitor-panel/node-monitor-panel-component.directive";
+} from '@/app/components/node-monitor-panel/node-monitor-panel-component.directive';
 
 @Component({
   selector: 'app-iclisten-data-collection-config-monitor-panel',
@@ -23,7 +26,7 @@ export class ICListenDataCollectionConfigMonitorPanel extends MutableNodeMonitor
 }> {
   waveformSampleRates = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000];
   spectrumSampleRates = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000];
-  bitDepths = [ // 16, 24, 32
+  bitDepths = [
     {label: '16', value: 2},
     {label: '24', value: 3},
     {label: '32', value: 4}
@@ -35,18 +38,61 @@ export class ICListenDataCollectionConfigMonitorPanel extends MutableNodeMonitor
   ];
   gainsDB = [0, 6, 12, 18, 24, 30, 36, 42, 48];
 
-  getTitle(): string {
-    return "iclisten-logging-config";
-  }
-
   constructor() {
     super();
   }
 
-  applySettings() {
-
+  getTitle(): string {
+    return 'iclisten-logging-config';
   }
 
-  protected readonly console = console;
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWavLoggingMode(value: number) {
+    this.data.loggingConfig.wav.loggingMode = value;
+  }
 
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWavSampleRate(value: number) {
+    this.data.loggingConfig.wav.sampleRate = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWavLogLength(value: number) {
+    this.data.loggingConfig.wav.logLength = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWavGain(value: number) {
+    this.data.loggingConfig.wav.gain = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWavBitDepth(value: number) {
+    this.data.loggingConfig.wav.bitDepth = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFftLoggingMode(value: number) {
+    this.data.loggingConfig.fft.loggingMode = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFftSampleRate(value: number) {
+    this.data.loggingConfig.fft.sampleRate = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFftLogLength(value: number) {
+    this.data.loggingConfig.fft.logLength = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFftProcessingType(value: any) {
+    this.data.loggingConfig.fft.processingType = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFftAccumulations(value: number) {
+    this.data.loggingConfig.fft.fftsAccumulated = value;
+  }
 }
--- src/app/components/node-monitor-panel/pam-modules/iclisten/iclisten-streaming-config-monitor-panel/iclisten-streaming-config-monitor-panel.component.ts
+++ src/app/components/node-monitor-panel/pam-modules/iclisten/iclisten-streaming-config-monitor-panel/iclisten-streaming-config-monitor-panel.component.ts
@@ -1,14 +1,15 @@
+// iclisten-streaming-config-monitor-panel.component.ts (updated)
 import {Component} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
-import {TranslateModule} from "@ngx-translate/core";
-import {TooltipComponent} from "../../../../shared/addons/tooltip/tooltip.component";
-import {undoable} from "@/app/services/pop-ups/undo-popup-service/undoable-decorator";
-import {ICListenStreamingConfig} from "@/app/global-interfaces/nodes/PamModules";
-import {
-  MutableNodeMonitorPanelComponent
-} from "@/app/components/node-monitor-panel/node-monitor-panel-component.directive";
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {TooltipComponent} from '../../../../shared/addons/tooltip/tooltip.component';
+import {ICListenStreamingConfig} from '@/app/global-interfaces/nodes/PamModules';
 
+import {
+  EmitChangesAfter,
+  MutableNodeMonitorPanelComponent
+} from '@/app/components/node-monitor-panel/node-monitor-panel-component.directive';
 
 @Component({
   selector: 'app-iclisten-streaming-config-monitor-panel',
@@ -29,24 +30,23 @@ export class ICListenStreamingConfigMonitorPanelComponent extends MutableNodeMon
   spectrumSampleRates = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000];
   bitDepths = [16, 24, 32];
   wavProcessingTypes = [
-    {value: 0, label: 'None'},
-    {value: 1, label: 'Teager-Kaiser'}
+    { value: 0, label: 'None' },
+    { value: 1, label: 'Teager-Kaiser' }
   ];
-
   fftProcessingTypes = [
-    {value: 0, label: 'None'},
-    {value: 1, label: 'Magnitude-Calculation'}
+    { value: 0, label: 'None' },
+    { value: 1, label: 'Magnitude-Calculation' }
   ];
 
   showTooltip: { [key: string]: boolean } = {};
   private tooltipTimers: { [key: string]: any } = {};
 
-  getTitle(): string {
-    return "iclisten-iclisten-streaming-config-monitor-panel";
-  }
-
   constructor() {
     super();
+  }
+
+  getTitle(): string {
+    return 'iclisten-iclisten-streaming-config-monitor-panel';
   }
 
   onMouseEnter(field: string) {
@@ -60,20 +60,59 @@ export class ICListenStreamingConfigMonitorPanelComponent extends MutableNodeMon
     this.showTooltip[field] = false;
   }
 
-  @undoable(2000)
-  applySettings() {
-    console.log('Applying settings:', {
-      waveformConfig: this.data.streamingConfig.wav,
-      spectrumConfig: this.data.streamingConfig.fft
-    });
-
-  }
-
   isDurationOrPeriodValid(duration: number) {
-    // Duration must be between 0 and 65535
     return duration >= 0 && duration <= 65535;
   }
 
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setRecordWaveform(value: boolean) {
+    this.data.streamingConfig.wav.recordWaveform = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setProcessWaveform(value: boolean) {
+    this.data.streamingConfig.wav.processWaveform = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWaveformProcessingType(value: number) {
+    this.data.streamingConfig.wav.waveformProcessingType = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWaveformInterval(value: number) {
+    this.data.streamingConfig.wav.waveformInterval = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setWaveformDuration(value: number) {
+    this.data.streamingConfig.wav.waveformDuration = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setRecordFFT(value: boolean) {
+    this.data.streamingConfig.fft.recordFFT = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setProcessFFT(value: boolean) {
+    this.data.streamingConfig.fft.processFFT = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFFTProcessingType(value: number) {
+    this.data.streamingConfig.fft.fftProcessingType = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFFTInterval(value: number) {
+    this.data.streamingConfig.fft.fftInterval = value;
+  }
+
+  @EmitChangesAfter(self => self.emitChange(self.data))
+  setFFTDuration(value: number) {
+    this.data.streamingConfig.fft.fftDuration = value;
+  }
 
   protected readonly console = console;
 }
