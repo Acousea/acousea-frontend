72f2b6a9ae20b837d190515db2e3ced5d6ea0ba7
antonioap101 <antonioap101@users.noreply.github.com>
2025-04-09 00:42:04 +0100

    update(DotEnv plugin): Updated DotEnv plugin path to .env file

--- custom-webpack.config.ts
+++ custom-webpack.config.ts
@@ -3,7 +3,8 @@ import Dotenv from "dotenv-webpack";
 module.exports = {
   plugins: [new Dotenv(
     {
-      systemvars: true
+      systemvars: true,
+      path: "./.env", // Path to .env file (this is the default)
     }
   )],
 };
